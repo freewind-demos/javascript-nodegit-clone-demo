b120c3e663553674e26badecee63dbd4cece5542
freewind <nowindlee@gmail.com>
2018-09-07 14:36:32 +0800

    improve with .nvmrc and demo code

--- demo.js
+++ demo.js
@@ -1,6 +1,12 @@
-const Git = require('nodegit');
+const Git = require('nodegit')
 
-Git.Clone('https://github.com/freewind-demos/javascript-frontend-hello-world-demo.git', './local-repo/').
-then(function(repo) {
-    console.log('clone to: ./local-repo');
-});
+Git.Clone('https://github.com/freewind-demos/javascript-frontend-hello-world-demo.git', './local-repo/javascript-frontend-hello-world-demo')
+    .then(function (repo) {
+        return repo.getBranchCommit('master')
+    })
+    .then(function (commit) {
+        return commit.message()
+    })
+    .then(function (message) {
+        console.log('latest commit: ' + message)
+    })
