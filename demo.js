d120a52acf147392118379aad5c9a75af6e6c179
freewind <nowindlee@gmail.com>
2018-09-07 14:38:50 +0800

    add log message

--- demo.js
+++ demo.js
@@ -2,11 +2,13 @@ const Git = require('nodegit')
 
 Git.Clone('https://github.com/freewind-demos/javascript-frontend-hello-world-demo.git', './local-repo/javascript-frontend-hello-world-demo')
     .then(function (repo) {
+        console.log('cloned to: ./local-repo/javascript-frontend-hello-world-demo')
         return repo.getBranchCommit('master')
     })
     .then(function (commit) {
+        console.log('Got master latest commit: ' + commit)
         return commit.message()
     })
     .then(function (message) {
-        console.log('latest commit: ' + message)
+        console.log('latest commit message: ' + message)
     })
